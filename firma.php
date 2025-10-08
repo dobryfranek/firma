67458b6ae5720f395b9eb08658e98a44a1a8e0da
hacker <a.jakubowska@t1piaseczno.edu.pl>
2025-10-08 11:30:46 +0200

    v0.0.2

--- firma.php
+++ firma.php
@@ -7,31 +7,55 @@
     <link rel="stylesheet" href="firma.css">
 </head>
 <body>
-    
+
+<?php
+
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+$conn = new mysqli(SERVERNAME, USERNAME, PASSWORD);
+
+
+
+function get_table() {
+    $sql = "SELECT imie, nazwisko, pensja FROM pracownicy WHERE data_urodzenia < '1975-01-01'";
+    return "";
+}
+
+function get_ordered_list() {
+    return "";
+}
+
+function get_income() {
+    return "";
+}
+
+?>
+
 <main>
+    <header>
+        <h1>Baza danych o pracownikach</h1>
+    </header>
 
-<header>
-    <h1>Baza danych o pracownikach</h1>
-</header>
+    <span style="display: flex; flex-direction: row">
+    <div class="left">
+        <h2>Informatycy poniżej roku 1975</h2>
 
+        <table>
+        <?php echo get_table()?>   
+        </table>
+    </div>
 
-<div class="left">
-    <h2>Informatycy poniżej roku 1975</h2>
-
-    <table>
-    <?php get_table()?>   
-    </table>
-</div>
-
-<div class="right">
-    <h2>Sekretarki firmy "Omega"</h2>
-    <ol>
-        <?php get_ordered_list()?>
-    </ol>
-    <hr>
-    <?php get_income()?>
-</div>
-
+    <div class="right">
+        <h2>Sekretarki firmy "Omega"</h2>
+        <ol>
+            <?php echo get_ordered_list();?>
+        </ol>
+        <hr>
+        <?php echo get_income();?>
+    </div>
+    </span>
 </main>
 
 <footer>
